3738c3569edc87e97a35a926b74d4e28f3d7866f
Aniket Kumar <aa6443@srmist.edu.in>
2024-03-01 21:09:18 +0530

    create-react-app and vitereact difference and similarities

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,7 +1,10 @@
-
+import Chai from './Chai'
 function App() {
   return (
+    <>
+    <Chai />
     <h1>Chai aur react | Aniket kumar</h1>
+    </>
   );
 }
 
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,12 @@
-
+import Chai from "./chai"
 function App() {
 
   return (
-    <h1>Chai aur React | Aniket Kumar </h1>
+    <>
+      <Chai/>
+      <h1>chai aur react </h1>
+      <p>test para</p>
+    </>
   )
 }
 
